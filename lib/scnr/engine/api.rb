3b5cae075beb5fce41a0e7e2bead71d28cbe7ee7
Tasos Laskos <tasos.laskos@gmail.com>
2021-06-15 15:18:56 +0300

    API: Added #run!

--- lib/scnr/engine/api.rb
+++ lib/scnr/engine/api.rb
@@ -5,6 +5,26 @@ module Engine
 
 DSeL::DSL::Nodes::APIBuilder.build :API, namespace: self do
     import_relative_many 'api/*'
+
+    define :run!
+
+    def_run! do |&block|
+        SCNR::Engine::Framework.safe do |framework|
+            framework.checks.load SCNR::Engine::Options.checks
+
+            framework.plugins.load_defaults
+            framework.plugins.load SCNR::Engine::Options.plugins.keys
+
+            framework.run do
+                block.call(
+                  Hash[framework.sitemap.sort_by{ |u,_| u }],
+                  SCNR::Engine::Data.issues.sort,
+                  framework.statistics
+                )
+            end
+        end
+    end
+
 end
 
 end
