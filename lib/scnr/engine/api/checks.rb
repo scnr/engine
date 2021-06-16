bffb2e6511cfb5076d2f15ca38a93c82b838591c
Tasos Laskos <tasos.laskos@gmail.com>
2021-06-16 18:24:57 +0300

    API: Implemented adding components via conf

--- lib/scnr/engine/api/checks.rb
+++ lib/scnr/engine/api/checks.rb
@@ -1,6 +1,16 @@
 child :checks, :Checks do
+    UnsafeFramework = SCNR::Engine::Framework.unsafe
+
     define :as
 
-    def_as do |shortname, &block|
+    def_as do |shortname, info = {}, &block|
+        shortname = shortname.to_s
+
+        check = Class.new( SCNR::Engine::Check::Base )
+        check.shortname = shortname
+        check.define_method :run, &block
+        check.define_singleton_method :info, &proc { info }
+
+        UnsafeFramework.checks[shortname] = check
     end
 end
--- lib/scnr/engine/api/fingerprinters.rb
+++ lib/scnr/engine/api/fingerprinters.rb
@@ -1,6 +1,16 @@
 child :fingerprinters, :Fingerprinters do
+    UnsafeFramework = SCNR::Engine::Framework.unsafe
+
     define :as
 
     def_as do |shortname, &block|
+        shortname = shortname.to_s
+
+        fingerprinter = Class.new( SCNR::Engine::Platform::Fingerprinter )
+        fingerprinter.define_method :run, &block
+        fingerprinter.define_method :shortname, &proc{ shortname }
+
+        SCNR::Engine::Platform::Manager.fingerprinters[shortname] =
+          fingerprinter
     end
 end
