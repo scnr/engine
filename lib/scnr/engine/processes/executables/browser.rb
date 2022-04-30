ba8eb613b188957543e481a4842c01debddc2278
zapotek <tasos.laskos@gmail.com>
2022-04-30 23:16:25 +0300

    browser executable: Revert to host libraries

--- lib/scnr/engine/processes/executables/browser.rb
+++ lib/scnr/engine/processes/executables/browser.rb
@@ -20,6 +20,14 @@ rescue EOFError, Errno::EPIPE => e
     true
 end
 
+if ENV['HOST_LD_LIBRARY_PATH']
+    ENV['LD_LIBRARY_PATH'] = ENV['HOST_LD_LIBRARY_PATH']
+end
+
+if ENV['HOST_LIBRARY_PATH']
+    ENV['LIBRARY_PATH'] = ENV['HOST_LIBRARY_PATH']
+end
+
 # Get a clean slate every time.
 ENV['TMPDIR'] = "#{$options[:tmpdir]}/#{File.basename( $options[:executable] )}_#{Process.pid}"
 
